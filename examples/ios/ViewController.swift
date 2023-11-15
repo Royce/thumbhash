0569f96d0e2f3d936b8395f5333c2feebdaaaa2d
Royce Townsend <github@roycetownsend.com>
2023-11-15 15:42:38 +0800

    An example that breaks.

--- examples/ios/ViewController.swift
+++ examples/ios/ViewController.swift
@@ -7,10 +7,14 @@ class ViewController: UIViewController {
     let image = UIImage(imageLiteralResourceName: "flower.jpg")
 
     // Image to ThumbHash
-    let thumbHash = imageToThumbHash(image: image)
+    let thumbHashGood = imageToThumbHash(image: image)
+    let thumbHashBadString = "CwgGH4Z3q3cgd2SkW3mHqVh3R8YJZYs="
+    guard let thumbHashBad = Data(base64Encoded: thumbHashBadString, options: .ignoreUnknownCharacters) else {
+        return
+    }
 
     // ThumbHash to image
-    let placeholder = thumbHashToImage(hash: thumbHash)
+    let placeholder = thumbHashToImage(hash: thumbHashBad)
 
     // Simulate setting the placeholder first, then the full image loading later on
     let view = UIImageView(image: placeholder)
@@ -19,8 +23,8 @@ class ViewController: UIViewController {
     view.frame = CGRect(x: 20, y: self.view.safeAreaInsets.top + 20, width: 150, height: 200)
     self.view.addSubview(view)
 
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-      view.image = image
-    }
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//      view.image = image
+//    }
   }
 }
